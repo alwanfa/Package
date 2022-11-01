757c126d826ef2030a6a9faee12e44b8894c0c86
WanDev <71076451+alwanfa@users.noreply.github.com>
2022-11-01 10:56:32 +0700

    innit commit

---
+++ app/Models/Manusia.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Manusia extends Model
+{
+    use HasFactory;
+}
